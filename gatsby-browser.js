a1d4b44c5874e94e587529a67186cfb8f65aac7c
Taryn <taryn@taryn.codes>
2020-04-30 15:29:19 +1000

    feat(auth): remove auth0
    This commit removes auth0 because I've decided to try a different approach using firebase.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,2 @@
-import React, { useState, useEffect } from "react"
-import { checkSession } from "./src/utils/auth"
+
 import "./src/styles/global.scss"
-
-const SessionCheck = ({ children }) => {
-  const [loading, stillLoading] = useState(true);
-  useEffect(() => checkSession(() => stillLoading(false)));
-  return loading === false && <>{children}</>
-};
-
-export const wrapRootElement = ({ element }) => (
-  <SessionCheck>{element}</SessionCheck>
-);
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,35 +1,7 @@
-/**
- * Top navigation component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
 import { Link } from "gatsby"
-import { logout, isAuthenticated } from "../utils/auth"
-import { Router } from "@reach/router"
-
 const Navigation = () => {
-  const Home = () => <p>Home</p>
-  const MyPet = () => <p>My Pet</p>
-  const Settings = () => <p>Settings</p>
-  const Games = () => <p>Games</p>
 
-  if (!isAuthenticated()) {
-    console.log('not authenticated');
-    return (
-      <nav id="navigation" className="header__nav">
-        <ul id="menu">
-          <li>
-            <Link to="/account/">Log in / Sign up</Link>
-          </li>
-        </ul>
-      </nav>
-    )
-  }
-
-  console.log('is authenticated');
   return (
     <>
       <nav>
@@ -37,22 +9,7 @@ const Navigation = () => {
         <Link to="/account/">My Pet</Link>{" "}
         <Link to="/account/games/">Games</Link>{" "}
         <Link to="/account/settings/">Settings</Link>{" "}
-        <a
-          href="#logout"
-          onClick={e => {
-            logout()
-            e.preventDefault()
-          }}
-        >
-          Log Out
-        </a>
     </nav>
-    <Router>
-      <Home path="/" />
-      <MyPet path="/account/" />
-      <Settings path="/account/settings" />
-      <Games path="/account/games" />
-    </Router>
   </>
   )
 }
