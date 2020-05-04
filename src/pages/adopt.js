f12c17290512784ff64b8ba99ce0046f2d0117b4
Taryn <taryn@taryn.codes>
2020-05-04 14:41:14 +1000

    feat(adopt): create form for adoption page
    This commit creates the form for the adoption page and sets up the data structure, but does not yet connect it to the database.

--- src/pages/adopt.js
+++ src/pages/adopt.js
@@ -1,10 +1,54 @@
-import React from "react"
+import React, {useState, useContext} from "react"
+import {FirebaseContext} from '../components/firebase'
 
 const Adopt = () => {
+  const {firebase} = useContext(FirebaseContext);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [formValues, setFormValues] = useState({
+    species: '',
+    name: '',
+    energy: 30,
+    fun: 10,
+    happiness: 10,
+    hunger: 10,
+    createdAt: new Date(),
+    user: 'Test'
+  });
+
+  function handleInputChange(e) {
+    e.persist();
+    setErrorMessage('');
+    setFormValues(currentValues => ({
+      ...currentValues,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('adopt form submitted');
+    console.log(formValues);
+  }
 
   return (
     <main>
       <p>Welcome to the adoption centre!</p>
+      <form onSubmit={handleSubmit}>
+        {!!errorMessage &&
+          <p id="errors" role="alert" aria-atomic="true" className="form__errorMessage">Error: {errorMessage}</p>
+        }
+        <fieldset>
+          <legend>Choose a pet:</legend>
+          <label htmlFor="puppy"><input id="puppy" type="radio" name="species" value="puppy" onChange={handleInputChange}/> Pug puppy</label>
+          <label htmlFor="axolotl"><input id="axolotl" type="radio" name="species" value="axolotl" onChange={handleInputChange}/>Axolotl</label>
+          <label htmlFor="rat"><input id="rat" type="radio" name="species" value="rat" onChange={handleInputChange}/>Rat</label>
+          <label htmlFor="kitten"><input id="kitten" type="radio" name="species" value="kitten" onChange={handleInputChange}/>Unicorn Kitten</label>
+        </fieldset>
+        <label htmlFor="name">Name your pet: <input id="name" name="name" type="text" placeholder="name" onChange={handleInputChange} required/></label>
+        <button type="submit">
+          Adopt your CheesePet
+        </button>
+      </form>
     </main>
   );
 }
