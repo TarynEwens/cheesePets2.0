cc8a7f66cd0de5727c1a6c0e06c90fd5751ecff2
Taryn <taryn@taryn.codes>
2020-06-14 18:41:18 +1000

    feat(adoption): styles
    This commit adds the styles for the pet adoption page which is part of the registration process. It also adjuts the markup to allow for the styles to work correctly.

--- src/pages/adopt.js
+++ src/pages/adopt.js
@@ -42,23 +42,72 @@ const Adopt = () => {
         <title>CheesePets: Adopt a pet</title>
         <html lang="en" />
       </Helmet>
-      <h2>Welcome to the CheeseLand Adoption Centre!</h2>
-      <form onSubmit={handleSubmit}>
-        {!!errorMessage &&
-          <p id="errors" role="alert" aria-atomic="true" className="form__errorMessage">Error: {errorMessage}</p>
-        }
-        <fieldset>
-          <legend>Choose a pet:</legend>
-          <label htmlFor="puppy"><input id="puppy" type="radio" name="species" value="Puppy" onChange={handleInputChange}/> Pug</label>
-          <label htmlFor="axolotl"><input id="axolotl" type="radio" name="species" value="Axolotl" onChange={handleInputChange}/>Axolotl</label>
-          <label htmlFor="rat"><input id="rat" type="radio" name="species" value="Rat" onChange={handleInputChange}/>Rat</label>
-          <label htmlFor="kitten"><input id="kitten" type="radio" name="species" value="Kitten" onChange={handleInputChange}/>Kitten</label>
-        </fieldset>
-        <label htmlFor="name">Name your pet: <input id="name" name="name" type="text" placeholder="name" onChange={handleInputChange} value={formValues.name} required/></label>
-        <button type="submit" className={"button button--cta"}>
-          Adopt your CheesePet
-        </button>
-      </form>
+      <section className={"petProfile"}>
+        <div className={"adoption__content"}>
+          <h2>Welcome to the CheesePets Adoption Centre!</h2>
+          <form onSubmit={handleSubmit} className="adoption__form">
+            {!!errorMessage &&
+              <p id="errors" role="alert" aria-atomic="true" className="form__errorMessage">Error: {errorMessage}</p>
+            }
+            <fieldset>
+              <legend>Choose a pet:</legend>
+              <div>
+              <label htmlFor="puppy" className={"adoption__label"}>
+                <img
+                  src={'/pets/puppy_neutral.png'}
+                  alt={`Cartoon Puppy`}
+                  className={"adoption__image"}
+                />
+                <span>
+                  <input id="puppy" type="radio" name="species" value="Puppy" onChange={handleInputChange}  className={"adoption__radioButton"} />
+                  Pug
+                </span>
+              </label>
+
+              <label htmlFor="axolotl" className={"adoption__label"}>
+                <img
+                  src={'/pets/axolotl_neutral.png'}
+                  alt={`Cartoon Axolotl`}
+                  className={"adoption__image"}
+                />
+                <span>
+                  <input id="axolotl" type="radio" name="species" value="Axolotl" onChange={handleInputChange} className={"adoption__radioButton"} />
+                  Axolotl
+                </span>
+              </label>
+
+              <label htmlFor="rat" className={"adoption__label"}>
+              <img
+                  src={'/pets/rat_neutral.png'}
+                  alt={`Cartoon Rat`}
+                  className={"adoption__image"}
+                />
+                <span>
+                  <input id="rat" type="radio" name="species" value="Rat" onChange={handleInputChange} className={"adoption__radioButton"} />
+                  Rat
+                </span>
+              </label>
+
+              <label htmlFor="kitten" className={"adoption__label"}>
+              <img
+                  src={'/pets/kitten_neutral.png'}
+                  alt={`Cartoon Kitten`}
+                  className={"adoption__image"}
+                />
+                <span>
+                  <input id="kitten" type="radio" name="species" value="Kitten" onChange={handleInputChange} className={"adoption__radioButton"} />
+                  Kitten
+                </span>
+              </label>
+              </div>
+            </fieldset>
+            <label htmlFor="name" className={"adoption__nameLabel"}>Name your pet: <input id="name" name="name" type="text" placeholder="name" onChange={handleInputChange} value={formValues.name} required/></label>
+            <button type="submit" className={"button button--cta"}>
+              Adopt your CheesePet
+            </button>
+          </form>
+        </div>
+      </section>
     </>
   );
 }
